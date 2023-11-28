6d71f80e867a8e191660f3fbf62cf30b1ed8c897
LeafarRz <152316134+LeafarRz@users.noreply.github.com>
2023-11-28 11:25:42 -0600

    Actualización barra_navegacion.vue
    Código documentado

--- barra_navegacion.vue
+++ barra_navegacion.vue
@@ -1,52 +1,36 @@
 <script setup>
+// Importa las funciones necesarias de vue-router y vue
 import { useRouter } from 'vue-router';
-import { ref } from 'vue';
 
+// Obtiene la instancia del enrutador
 const router = useRouter();
-const showMenu = ref(false);
 
+// Función para navegar a una ruta y ocultar el menú
 function GoRuta(ruta) {
+  // Muestra la ruta en la consola (puede ser útil para depuración)
   console.log(ruta);
+  // Navega a la ruta especificada utilizando vue-router
   router.push(ruta);
+  // Oculta el menú después de navegar
   showMenu.value = false;
 }
-
-function toggleMenu() {
-  showMenu.value = !showMenu.value;
-}
-
 </script>
 
-
 <template>
+  <!-- Contenedor principal del componente -->
   <div style="margin-top: 10px;">
-  <button id="btnRoutes" @click="GoRuta('/')">Inicio</button>
-  <button id="btnRoutes" @click="GoRuta('/cart')">Blog de publicaciones</button>
-  <button id="btnRoutes" @click="GoRuta('/')">Contacto</button>
-  <button id="btnRoutes" @click="GoRuta('/cart')">Perfil</button>
+    <!-- Botones que llaman a la función GoRuta con rutas específicas -->
+    <button id="btnRoutes" @click="GoRuta('/')">Inicio</button>
+    <button id="btnRoutes" @click="GoRuta('/cart')">Blog de publicaciones</button>
+    <button id="btnRoutes" @click="GoRuta('/')">Contacto</button>
+    <button id="btnRoutes" @click="GoRuta('/cart')">Perfil</button>
   </div>
 
-  <!-- <div>
-      <div class="menu-btn-container">
-        <div class="menu-btn" :class="{ 'menu-btn-active': showMenu }" @click="toggleMenu()">
-          <div class="btn-icon">&#9776;</div>
-        </div>
-      </div>
-
-      <div v-show="showMenu" class="overlay">
-        <div class="menu">
-          <ul>
-            <li><a @click="GoRuta('/')">Inicio</a></li>
-            <li><a @click="GoRuta('/cart')">Blog de publicaciones</a></li>
-            <li><a @click="GoRuta('/contacto')">Contacto</a></li>
-            <li><a @click="GoRuta('/perfil')">Perfil</a></li>
-          </ul>
-        </div>
-      </div>
-    </div> -->
+  
 </template>
 
 <style scoped>
+/* Estilos CSS aplicados al componente (scoped para limitar el alcance) */
 
 #btnRoutes {
   background-color: transparent;
@@ -56,61 +40,5 @@ function toggleMenu() {
   border: none;
   font-family: "Roboto", sans-serif;
 }
-/* 
 
-.menu-btn-container {
-  position: fixed;
-  top: 20px;
-  right: 20px;
-  z-index: 2; 
-}
-
-.menu-btn {
-  cursor: pointer;
-}
-
-.menu-btn-active {
-  color: #fff; 
-}
-
-.btn-icon {
-  font-size: 24px;
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.9); 
-  z-index: 1;
-}
-
-.menu {
-  position: fixed;
-  top: 0;
-  right: 0;
-  width: 300px;
-  height: 100%;
-  background-color: #000;
-  padding: 20px;
-  box-sizing: border-box;
-}
-
-.menu ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-.menu li {
-  margin-bottom: 15px;
-}
-
-.menu a {
-  text-decoration: none;
-  color: #fff;
-  font-size: 18px;
-} */
 </style>
