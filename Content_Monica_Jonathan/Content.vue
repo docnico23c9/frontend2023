bc719c60ce7a5c3276c7615d02363ad29fdbe283
MonicaFloresZ <124377422+MonicaFloresZ@users.noreply.github.com>
2023-11-30 02:04:03 -0600

    Componente de Contenido

---
+++ Content_Monica_Jonathan/Content.vue
@@ -0,0 +1,312 @@
+<script setup>
+
+</script>
+
+<!-- <template>
+
+    <div style="background-color: white; justify-content: center;">
+        <br>
+        <h3 class="titulo">Titulo 1 plantilla</h3>
+        <br>
+        <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi iste voluptates
+            doloremque neque sit, harum adipisci, at assumenda soluta impedit pariatur, hic illo dolore vitae. Eveniet
+            exercitationem eos corrupti?</p>
+        <br><br>
+        <div class="contenedor1">
+
+            <div class="contenedor1Div1">
+                <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                    style="width: 100%;">
+                <h2>Subtítulo descriptivo</h2>
+                <p>Párrafo descriptivo</p>
+                <button>Texto</button>
+            </div>
+
+        </div>
+        <br><br>
+        <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi iste voluptates
+            doloremque neque sit, harum adipisci, at assumenda soluta impedit pariatur, hic illo dolore vitae. Eveniet
+            exercitationem eos corrupti?</p>
+        <br><br>
+    </div>
+
+    <div style="background-color: white; margin-right: 0;">
+        <br>
+        <h3 class="titulo">Titulo 2 plantilla</h3>
+        <br>
+        <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi iste voluptates
+            doloremque neque sit, harum adipisci, at assumenda soluta impedit pariatur, hic illo dolore vitae. Eveniet
+            exercitationem eos corrupti?</p>
+        <br><br>
+        <div class="contenedor1">
+
+            <div class="contenedor1Div1">
+                <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                    style="width: 100%;">
+                <h2>Subtítulo descriptivo</h2>
+                <p>Párrafo descriptivo</p>
+                <button>Texto</button>
+            </div>
+
+        </div>
+        <br><br>
+        <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi iste voluptates
+            doloremque neque sit, harum adipisci, at assumenda soluta impedit pariatur, hic illo dolore vitae. Eveniet
+            exercitationem eos corrupti?</p>
+        <br><br>
+</div></template> -->
+
+<template>
+    <!-- Contenedor principal que tiene todo el contenido de template -->
+    <div class="contenedorPrincipal">
+
+        <!-- Primer subcontenedor que da acomodo a la primer sección superior del contenido -->
+        <div class="subcontendor">
+            <h3 class="titulo">Titulo 1 plantilla</h3>
+            <br> <br>
+            <p class="parrafo">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi
+                iste voluptates doloremque neque sit, harum adipisci, at assumenda soluta
+                impedit pariatur, hic illo dolore vitae. Eveniet exercitationem eos
+                corrupti?
+            </p>
+            <br><br>
+            <!-- Este contenedor interno da formato a las cards en acomodo en fila -->
+            <div class="contenedor1">
+
+                <!-- Este contenedor es específico del contenido de cada card -->
+                <div class="contenedor1Div1">
+                    <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                        style="width: 100%;" />
+                    <h2>Subtítulo descriptivo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus modi ullam totam esse doloribus
+                        voluptates ab architecto ipsa nobis voluptate? Eum animi tenetur cupiditate totam aspernatur nisi
+                        explicabo perspiciatis repudiandae.</p>
+                    <button>Texto botón</button>
+                </div>
+
+                <!-- Este contenedor es específico del contenido de cada card -->
+                <div class="contenedor1Div1">
+                    <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                        style="width: 100%;" />
+                    <h2>Subtítulo descriptivo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae pariatur, rerum iure
+                        necessitatibus eum odit dolores aspernatur. Rem animi earum, temporibus a quia quasi sunt vero modi
+                        neque voluptas. Necessitatibus?</p>
+                    <button>Texto botón</button>
+                </div>
+            </div>
+            <br><br>
+            <p class="parrafo">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi
+                iste voluptates doloremque neque sit, harum adipisci, at assumenda soluta
+                impedit pariatur, hic illo dolore vitae. Eveniet exercitationem eos
+                corrupti?
+            </p>
+            <br><br>
+        </div>
+
+        <!-- Segundo subcontenedor que da acomodo a la segunda sección superior del contenido -->
+        <div class="subcontenedor">
+
+            <h3 class="titulo">Subtítulo</h3>
+            <br>
+            <!-- Este contenedor interno da formato a las cards en acomodo en fila -->
+            <div class="contenedor2">
+
+                <!-- Este contenedor es específico del contenido de cada card -->
+                <div class="contenedor2Div2">
+                    <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                        style="width: 100%;">
+                    <h2>Subtítulo descriptivo</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio deleniti est impedit magni nulla
+                        accusamus explicabo cum excepturi dolor adipisci! Error cumque, accusamus deserunt rerum dolore
+                        natus culpa aliquid corrupti?</p>
+                    <button>Texto botón</button>
+                </div>
+
+                <!-- Este contenedor es específico del contenido de cada card -->
+                <div class="contenedor2Div2">
+                    <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                        style="width: 100%;">
+                    <h2>Subtítulo descriptivo</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat sequi reiciendis aspernatur hic
+                        quae iure doloribus ratione amet dolores exercitationem minima, aliquid officia, labore atque. Magni
+                        consequuntur itaque ex?</p>
+                    <button>Texto botón</button>
+                </div>
+
+                <!-- Este contenedor es específico del contenido de cada card -->
+                <div class="contenedor2Div2">
+                    <img src="https://i0.wp.com/www.ntv.com.mx/wp-content/uploads/2019/11/golden-cachorro-e1549967733842-1024x650.jpg?fit=1024%2C650&ssl=1"
+                        style="width: 100%;">
+                    <h2>Subtítulo descriptivo</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fugiat sequi reiciendis aspernatur hic
+                        quae iure doloribus ratione amet dolores exercitationem minima, aliquid officia, labore atque. Magni
+                        consequuntur itaque ex?</p>
+                    <button>Texto botón</button>
+                </div>
+
+            </div>
+
+            <br><br>
+
+            <p class="parrafo">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi
+                iste voluptates doloremque neque sit, harum adipisci, at assumenda soluta
+                impedit pariatur, hic illo dolore vitae. Eveniet exercitationem eos
+                corrupti?
+            </p>
+        </div>
+
+
+
+    </div>
+</template>
+  
+  <script setup>
+  </script>
+  
+<style>
+/*Estilos del contenedor principal que tiene todo el contenido de template */
+.contenedorPrincipal {
+    /*Disposición flex column para acomodar las cards en fila */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 1000px;
+    padding: 20px;
+    box-sizing: border-box;
+    /* Color de fondo del componente */
+    background-color: rgb(226, 217, 248);
+}
+
+/*Subcontenedor que contiene la primer sección de dos cards y también la segunda sección de tres cards*/
+.subcontenedor {
+    width: 100%;
+    /*Ajuste del tamaño de ancho de las cards en fila*/
+    max-width: 1000px;
+    margin-bottom: 20px;
+    border-radius: 10px;
+    padding: 20px;
+    box-sizing: border-box;
+}
+
+/*Estilos para el título*/
+.titulo {
+    font-size: 25px;
+    color: black;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    text-align: center;
+}
+
+/*Estilos para los párrafos que no son los de las cards*/
+.parrafo {
+    font-size: 16px;
+    font-weight: 600;
+    text-transform: none;
+    margin-left: 10px;
+    color: black;
+    text-align: justify;
+    margin-left: 50px;
+    margin-right: 50px;
+}
+
+/*Estilos del contenedor interno que contiene como tal las cards y les da el acomodo en fila*/
+.contenedor1,
+.contenedor2 {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    /*Se define la anchura total que deben abarcar las cards en la pantalla*/
+    width: 95%;
+    align-items: center;
+    justify-content: center;
+    margin-left: 10px;
+}
+
+/*Estilos del contenedor que da formato a cada card*/
+.contenedor1Div1 {
+    width: 45%;
+    margin-left: 25px;
+    background-color: white;
+    border-radius: 10px;
+    padding: 10px;
+    text-align: center;
+    box-shadow: 2px 2px 7px rgb(185, 185, 185);
+    border: 1px rgb(236, 236, 236);
+}
+
+.contenedor1Div1 img {
+    width: 20%;
+}
+
+.contenedor1Div1 h2 {
+    font-size: 20px;
+    font-weight: 700;
+    margin: 10px 0;
+    color: black;
+}
+
+/*Estilo para los párrafos de las primeras dos cards*/
+.contenedor1Div1 p {
+    font-size: 16px;
+    margin: 0 0 10px 0;
+    color: black;
+}
+
+/*Estilos de los botones de las cards del primer contenedor*/
+.contenedor1Div1 button {
+    font-size: 15px;
+    font-weight: 700;
+    background-color: white;
+    border: 1px solid #000;
+    border-radius: 20px;
+    width: 200px;
+    height: 30px;
+    margin: 10px 0;
+    cursor: pointer;
+}
+
+/*Estilos del contenedor que da formato a cada card*/
+.contenedor2Div2 {
+    width: 30%;
+    background-color: white;
+    box-shadow: 2px 2px 7px rgb(225, 225, 225);
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+    text-align: center;
+}
+
+.contenedor2Div2 img {
+    width: 100%;
+}
+
+.contenedor2Div2 h2 {
+    font-size: 16px;
+    font-weight: 700;
+    margin: 10px 0;
+    color: black;
+}
+
+/*Estilo de los párrafos de las últimas tres cards en fila*/
+.contenedor2Div2 p {
+    font-size: 16px;
+    margin: 0 0 10px 0;
+    color: rgb(0, 0, 0);
+}
+
+/*Estilos de los botones de las cards del segundo contenedor*/
+.contenedor2Div2 button {
+    font-size: 12px;
+    font-weight: 700;
+    background-color: white;
+    border: 1px solid #000;
+    border-radius: 20px;
+    width: 120px;
+    height: 30px;
+    margin: 10px 0;
+}
+</style>
