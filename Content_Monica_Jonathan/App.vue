48cf737a4a0aa52df3c27506538d200d5b6c557f
MonicaFloresZ <124377422+MonicaFloresZ@users.noreply.github.com>
2023-11-30 01:54:03 -0600

    Componente de Contenido

---
+++ Content_Monica_Jonathan/App.vue
@@ -0,0 +1,20 @@
+
+
+<script setup>
+import Content from './components/Content.vue';
+</script>
+
+
+<template >
+  <div id="app">
+
+  <Content/> 
+    
+  </div>
+</template>
+
+
+<style>
+
+</style>
+
